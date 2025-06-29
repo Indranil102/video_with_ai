eb65a296a9105d4c1d85e521b2a46d3f0e93c746
Indranil102e <indranilsamanta2003@gmail.com>
2025-06-29 11:39:56 +0530

    feat: implement authentication options with credentials provider

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,1 +1,52 @@
-export
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials"
+import { connectToDatabase } from "./db";
+import User from "@/models/User";
+import bcrypt from "bcryptjs";
+
+export const authOptions:NextAuthOptions={
+    providers: [
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                email: { label: "Email", type: "email"},
+                password: { label: "Password", type: "password" },
+                
+            },  
+
+            async authorize(credentials) {
+                if(!credentials?.email || !credentials?.password){
+                    throw new Error("Email and password are required.");
+                }
+
+                try {
+                    await connectToDatabase()
+                   const user= await User.findOne({email:credentials.email})
+
+                   if(!user){
+                    throw new Error("No user found with this email.");
+                   }
+                  const isValid= await bcrypt.compare(
+                    credentials.password,
+                    user.password
+                   )
+
+                   if(!isValid){
+                    throw new Error("Invalid Password.");
+                   }
+                    
+                   return{
+                    id:user._id.toString(),
+                    email:user.email,
+                    
+                   }
+                   
+                } catch (error) {
+                    console.error("Authorization Error", error);
+                    throw error;
+                    
+                }
+            } // Close authorize method
+        }) // Close CredentialsProvider
+    ] // Close providers array
+} // Close authOptions object
