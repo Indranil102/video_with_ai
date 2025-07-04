de1ecfcc4d4bd9f34310515aad111b32624fb423
Indranil102e <indranilsamanta2003@gmail.com>
2025-07-05 00:09:33 +0530

    feat: add error handling to ImageKit authentication route

--- api/auth/imagekit-auth/route.ts
+++ api/auth/imagekit-auth/route.ts
@@ -1,10 +1,9 @@
 // File: app/api/upload-auth/route.ts
 import { getUploadAuthParams } from "@imagekit/next/server"
 
-export async function GET() {
-    // Your application logic to authenticate the user
-    // For example, you can check if the user is logged in or has the necessary permissions
-    // If the user is not authenticated, you can return an error response
+try {
+    export async function GET() {
+    
 
     const authenticationParameters = getUploadAuthParams({
         privateKey: process.env.IMAGEKIT_PRIVATE_KEY as string, 
@@ -13,4 +12,12 @@ export async function GET() {
     })
 
     return Response.json({ authenticationParameters, publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY }, )
+    
+} catch (error) {
+
+    return Response.json({
+        error:"Authentication for ImageKit failed.",
+    } ,{ status: 500 })
+    
+}
 }
