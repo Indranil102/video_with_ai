545be402a65ca7b7fea60acfe5312d104cefc7f8
Indranil102e <indranilsamanta2003@gmail.com>
2025-07-06 18:07:09 +0530

    add Fileupload component for file upload functionality

---
+++ app/components/Fileupload.tsx
@@ -0,0 +1,38 @@
+"use client"
+
+import {
+    ImageKitAbortError,
+    ImageKitInvalidRequestError,
+    ImageKitServerError,
+    ImageKitUploadNetworkError,
+    upload,
+} from "@imagekit/next";
+import { useRef, useState } from "react";
+
+
+const Fileupload = () => {
+    
+    const [progress, setProgress] = useState(0);
+
+    const fileInputRef = useRef<HTMLInputElement>(null);
+
+    const abortController = new AbortController();
+
+    
+   
+    return (
+        <>
+            
+            <input type="file" ref={fileInputRef} />
+         
+            <button type="button" onClick={handleUpload}>
+                Upload file
+            </button>
+            <br />
+            
+            Upload progress: <progress value={progress} max={100}></progress>
+        </>
+    );
+};
+
+export default Fileupload;
