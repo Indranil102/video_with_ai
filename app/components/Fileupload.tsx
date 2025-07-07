08e323b0ee40164e0983d398d76298206b079bec
Indranil102e <indranilsamanta2003@gmail.com>
2025-07-07 12:48:16 +0530

    refactor: enhance Fileupload component with props for success and progress handling

--- app/components/Fileupload.tsx
+++ app/components/Fileupload.tsx
@@ -7,30 +7,55 @@ import {
     ImageKitUploadNetworkError,
     upload,
 } from "@imagekit/next";
+import { set } from "mongoose";
 import { useRef, useState } from "react";
 
+interface fileUploadProps{
+    onSuccess:(res:any)=>void
+    onProgress?:(progress:number)=>void
+    fileType?: "image" | "video" | "file";
+     
+}
 
-const Fileupload = () => {
+const Fileupload = ({onSuccess,onProgress,fileType}:fileUploadProps) => {
     
-    const [progress, setProgress] = useState(0);
-
-    const fileInputRef = useRef<HTMLInputElement>(null);
-
-    const abortController = new AbortController();
-
-    
+   const [uploading , setUploading] = useState(false);
+   const [error, setError] = useState<string,null>(null);
    
+   const validateFile= (file:File)=>{
+
+    if(fileType=== "video"){
+        if(!file.type.startsWith("video/")){
+            setError("Please upload a valid video file");
+    }
+
+   }
+
+   if(file.size > 100 * 1024 * 1024){ 
+        setError("File size exceeds 100 MB limit");
+   }
+
+   return true;
+}
+   
+
+
     return (
         <>
             
-            <input type="file" ref={fileInputRef} />
+            <input type="file" accept={
+                fileType === "video"
+                ? "video/*" 
+                : "image/*"}
+                onChange={handleFileChange}
+             
+            />
+
+            {
+                uploading && <p>Uploading...</p>
+            }
          
-            <button type="button" onClick={handleUpload}>
-                Upload file
-            </button>
-            <br />
             
-            Upload progress: <progress value={progress} max={100}></progress>
         </>
     );
 };
