97d62490657dce781993a8c7150b6a54d5166362
Indranil102e <indranilsamanta2003@gmail.com>
2025-07-08 16:39:52 +0530

    refactor: make headers optional in fetchOptions type; improve fetch method for better API request handling

--- lib/api-client.ts
+++ lib/api-client.ts
@@ -1,30 +1,48 @@
-type fetchOptions={
-    method?: "GET" | "POST" | "PUT" | "DELETE";
-    headers: Record<string, string>;
-    body?: any | null;
-}
+import { IVideo } from "@/models/Video";
+
+export type videoFormData = Omit<IVideo, "_id">; // omit is used to remove the _id field from the form d
+type fetchOptions = {
+  method?: "GET" | "POST" | "PUT" | "DELETE";
+  headers?: Record<string, string>; // made headers optional
+  body?: any | null;
+};
 
 class ApiClient {
-    private  async fetch<T>(
-        endpoint: string,
-        options: fetchOptions ={}
-    ): Promise<T>{
-        const { method = "GET", headers={}, body } = options;
+  private async fetch<T>(
+    endpoint: string,
+    options: fetchOptions = {}
+  ): Promise<T> {
+    const { method = "GET", headers = {}, body } = options;
 
-        const defaultHeaders = {
-            "Content-Type": "application/json",
-            ...headers
+    const defaultHeaders = {
+      "Content-Type": "application/json",
+      ...headers,
     };
 
-    const response = await this.fetch(`/api/${endpoint}`, {
+    const response = await fetch(
+      `/api${endpoint.startsWith("/") ? endpoint : "/" + endpoint}`,
+      {
         method,
         headers: defaultHeaders,
         body: body ? JSON.stringify(body) : undefined,
-    } 
-    )
+      }
+    );
+    if (!response.ok) {
+      throw new Error((await response.text()) || "Network response was not ok");
+    }
     return response.json();
+  }
 
-    
-    
-  
+  async getVideo() {
+    return this.fetch("/videos");
+  }
+
+  async createVideo(videoData: videoFormData) {
+    return this.fetch("/videos", {
+      method: "POST",
+      body: videoData,
+    });
+  }
 }
+
+export const apiClient = new ApiClient();
