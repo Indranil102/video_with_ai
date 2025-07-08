1afffa1b0a2d29af0a9eadf6ab7f13b7770eef6b
Indranil102e <indranilsamanta2003@gmail.com>
2025-07-08 15:47:24 +0530

    refactor: update fetchOptions type to allow any body type; enhance fetch method in ApiClient for improved API interaction

--- lib/api-client.ts
+++ lib/api-client.ts
@@ -1,5 +1,30 @@
 type fetchOptions={
     method?: "GET" | "POST" | "PUT" | "DELETE";
     headers: Record<string, string>;
-    body?: FormData;
+    body?: any | null;
+}
+
+class ApiClient {
+    private  async fetch<T>(
+        endpoint: string,
+        options: fetchOptions ={}
+    ): Promise<T>{
+        const { method = "GET", headers={}, body } = options;
+
+        const defaultHeaders = {
+            "Content-Type": "application/json",
+            ...headers
+    };
+
+    const response = await this.fetch(`/api/${endpoint}`, {
+        method,
+        headers: defaultHeaders,
+        body: body ? JSON.stringify(body) : undefined,
+    } 
+    )
+    return response.json();
+
+    
+    
+  
 }
